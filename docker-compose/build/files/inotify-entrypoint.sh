1fd9264cc7fe35284045db226ef19223fbf34d37
Tomasz Zok <tomasz.zok@cs.put.poznan.pl>
2020-10-29 20:12:36 +0100

    Enable runtime selection of KeyCloak config for Dashboard

--- docker-compose/build/files/inotify-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-java \
-    -jar /catalog_qt_2/client/catalog-ws-client/target/catalogAPI.jar \
-    -keyCloakServiceLogin \
-    --realm-settings-file /docker-entrypoint-properties.d/service-login.properties
-exec "$@"
