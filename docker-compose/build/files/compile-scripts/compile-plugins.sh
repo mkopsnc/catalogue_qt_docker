d22aa93c30576a89c963685a1215b1e4b2418c6f
mkowsiak <michal@owsiak.org>
2021-06-15 07:49:14 +0200

    Small change related to plugin compilation - we have to support three different scenarios

--- docker-compose/build/files/compile-scripts/compile-plugins.sh
+++ docker-compose/build/files/compile-scripts/compile-plugins.sh
@@ -19,3 +19,21 @@ if [ -e ${CATALOG_QT_HOME}/plugin/imas-feeder-plugin ]; then
 
 fi
 
+if [ -e ${CATALOG_QT_HOME}/plugin/imas-feeder-plugin-reader ]; then
+
+  cd ${CATALOG_QT_HOME}/plugin/imas-feeder-plugin-uri-parser
+  mvn install -DskipTests
+
+  cd ${CATALOG_QT_HOME}/plugin/imas-feeder-plugin-reader
+  mkdir -p local-maven-repo
+  mvn org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install-file \
+    -Dfile=`ls -1 /opt/imas/core/IMAS/*/jar/imas.jar | head -1` \
+    -DgroupId=imas \
+    -DartifactId=imas \
+    -Dversion=1.0.0-SNAPSHOT \
+    -Dpackaging=jar \
+    -DlocalRepositoryPath=`pwd`/local-maven-repo
+  mvn install -DskipTests
+
+fi
+
