7b0d4f7232a86934e63553478809c5c46133a94c
mkowsiak <michal@owsiak.org>
2021-06-15 23:04:55 +0200

    merged from refactor branch

---
+++ docker-compose/build/files/watchdog.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+java \
+    -jar /catalog_qt_2/client/catalog-ws-client/target/catalogAPI.jar \
+    -keyCloakServiceLogin \
+    --realm-settings-file /docker-entrypoint-properties.d/service-login.properties
+
+cd imas-watchdog
+
+wait-for-it server:8080 --timeout=0
+
+exec ./main.py --config imasdb.ini --verbose
