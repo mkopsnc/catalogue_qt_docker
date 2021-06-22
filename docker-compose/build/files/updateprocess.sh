a94f26f54351d7e255cc4143bbdbcde4223260c8
mkowsiak <michal@owsiak.org>
2021-06-22 10:24:45 +0200

    Support for disable-access-token feature

--- docker-compose/build/files/updateprocess.sh
+++ docker-compose/build/files/updateprocess.sh
@@ -6,13 +6,30 @@ CLIENT_WS_PROPERTIES=${CLIENT_WS_DIR}/src/main/resources/service-login.propertie
 
 wait-for-it server:8080 --timeout=0
 
-java -jar ${CLIENT_WS_JAR} \
+if [ -z "$DISABLE_ACCESS_TOKEN" ]; then
+
+  java -jar ${CLIENT_WS_JAR} \
     -keyCloakServiceLogin \
     --realm-settings-file ${CLIENT_WS_PROPERTIES} 
-
-exec java -jar ${CLIENT_WS_JAR} \
+  
+  exec java -jar ${CLIENT_WS_JAR} \
     -startUpdateProcess \
     --url http://server:8080 \
     --scheme mdsplus \
     --run-as-service \
     --slice-limit 100
+else
+
+  echo "DISABLE_ACCESS_TOKEN is set - access token will not be generated"
+
+  exec java -jar ${CLIENT_WS_JAR} \
+    -startUpdateProcess \
+    --url http://server:8080 \
+    --scheme mdsplus \
+    --run-as-service \
+    --disable-access-token \
+    --slice-limit 100
+
+fi
+
+
--- docker-compose/build/files/watchdog.sh
+++ docker-compose/build/files/watchdog.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
-java \
+
+if [ -z "$DISABLE_ACCESS_TOKEN" ]; then
+
+  cd imas-watchdog
+
+  wait-for-it server:8080 --timeout=0
+
+  exec ./main.py --config imasdb-no-token.ini --verbose
+
+else
+
+  java \
     -jar /catalog_qt_2/client/catalog-ws-client/target/catalogAPI.jar \
     -keyCloakServiceLogin \
     --realm-settings-file /docker-entrypoint-properties.d/service-login.properties
 
-cd imas-watchdog
+  cd imas-watchdog
 
-wait-for-it server:8080 --timeout=0
+  wait-for-it server:8080 --timeout=0
 
-exec ./main.py --config imasdb.ini --verbose
+  exec ./main.py --config imasdb.ini --verbose
+
+fi
+
+
