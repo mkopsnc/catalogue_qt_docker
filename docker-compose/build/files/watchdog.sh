10a0175e33712b2c375c4f86f0fa1255641926fa
Bartłomiej Pogodziński <bartlomiej.pogodzinski@gmail.com>
2021-06-25 12:43:27 +0200

    Cleanup

--- docker-compose/build/files/watchdog.sh
+++ docker-compose/build/files/watchdog.sh
@@ -9,7 +9,7 @@ INI_FILE=imasdb.ini
 if [ -z "$DISABLE_ACCESS_TOKEN" ]; then
 
   java \
-    -jar /catalog_qt_2/client/catalog-ws-client/target/catalogAPI.jar \
+    -jar /home/imas/catalog_qt_2/client/catalog-ws-client/target/catalogAPI.jar \
     -keyCloakServiceLogin \
     --realm-settings-file /docker-entrypoint-properties.d/service-login.properties
 
